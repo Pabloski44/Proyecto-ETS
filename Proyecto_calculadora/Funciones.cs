3e070616f847c8be8451c67720dc5c1ed14fb45f
Pabloski44 <101739898+Pabloski44@users.noreply.github.com>
2024-04-25 13:47:01 +0100

    Proyecto base

---
+++ Proyecto_calculadora/Funciones.cs
@@ -0,0 +1,16 @@
+﻿namespace Proyecto_calculadora
+{
+    public class Funciones
+    {
+        public void restar(int a , int b)
+        {
+            int calculo = a - b;
+            Console.WriteLine("La resta de {0} y {1} es : {3} ",a, b, calculo);
+        }
+        public void dividir(int a , int b)
+        {
+            int calculo = a / b;
+            Console.WriteLine("La division de {0} y {1} es : {3} ",a, b, calculo);
+        }
+    }
+}
---
+++ Proyecto_calculadora/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Proyecto_calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
